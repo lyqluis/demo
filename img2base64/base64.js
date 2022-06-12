c7bd2f53911d6dc581edb1a7b8a8c3851ef3fe6d
xiannvjiadexiaogouzi <GULA@LuisMacBookAir.local>
2022-06-12 19:53:00 +0800

    add img2base64

---
+++ img2base64/base64.js
@@ -0,0 +1,46 @@
+function getBase64(files) {
+  const file = files[0]
+  const reader = new FileReader()
+
+  return new Promise(resolve => {
+    reader.onloadend = function () {
+      resolve(reader.result)
+    }
+
+    reader.readAsDataURL(file)
+  })
+}
+
+// dom
+function getEl(el) {
+  return document.querySelector(el)
+}
+
+
+function showOutput(url, output, img, p) {
+  img.src = url
+  output.innerText = url
+  p.style.display = 'none'
+}
+
+function preventDefatult(e) {
+  e.stopPropagation()
+  e.preventDefault()
+}
+
+function hasClass(el, name) {
+  const classList = Array.from(el.classList)
+  return classList.includes(name)
+}
+
+function addClass(el, name) {
+  if (hasClass(el, name)) return
+  el.className = `${name} ${el.className}`
+}
+
+function removeClass(el, name) {
+  if (hasClass(el, name)) {
+    let classes = el.className
+    el.className = classes.replace(name, "")
+  }
+}
